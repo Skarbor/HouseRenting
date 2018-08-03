63f596969d563718c7722a3c72cd030a95ba8180
Adam Wójcik <adamwojcik.programista@gmail.com>
2018-08-03 15:09:00 +0200

    Added basic entity framework + identity

---
+++ server/HouseRenting/HouseRenting.DataClasses/Flat.cs
@@ -0,0 +1,8 @@
+﻿namespace HouseRenting.Models
+{
+    public class Flat
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ server/HouseRenting/HouseRenting.DbLayer/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HouseRenting.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HouseRenting.DbLayer.Context
+{
+    class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
+
+        public DbSet<Flat> Flats { get; set; }
+    }
+}
